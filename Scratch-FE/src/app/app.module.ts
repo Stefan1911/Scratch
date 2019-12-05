223cf70e2bdb20029096db9e7b23e040efd0b388
Stefan <stefan.bogosavljevic@elfak.rs>
2019-12-05 10:16:27 +0100

    feat CreatJS AB#15 : added the CreateJS library to the project

--- Scratch-FE/src/app/app.module.ts
+++ Scratch-FE/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppCanvasComponent } from './app-canvas/app-canvas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppCanvasComponent
   ],
   imports: [
     BrowserModule
